b2c3822b45c2ce21562d38d2bb581d51bc99a2fc
udaykiran <udaykiranramaraju@gmail.com>
2024-05-01 15:33:17 +0530

    react episode 1

---
+++ React/app.js
@@ -0,0 +1,10 @@
+// const heading = React.createElement("h1", {id:"heading"}, "Hello World from React!!")
+// console.log(heading);//at the end of the day react element is js obj..
+const heading = React.createElement(
+    'div',{id:"parent"},
+    React.createElement('div',{id:"child"},
+    [React.createElement('h1',{},"hello from react under 2divs'"), 
+    React.createElement('h2',{},"hello from react under 2divsn and one h1 tag'")]))
+console.log(heading)
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(heading)
