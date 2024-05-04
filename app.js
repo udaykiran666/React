0da06d9cc4494609c259944976da30678b19c397
udaykiran <udaykiranramaraju@gmail.com>
2024-05-05 01:40:41 +0530

    completed episode-2

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
+import React from "react";
+import ReactDOM  from "react-dom/client";
+
 // const heading = React.createElement("h1", {id:"heading"}, "Hello World from React!!")
 // console.log(heading);//at the end of the day react element is js obj..
 const heading = React.createElement(
     'div',{id:"parent"},
     React.createElement('div',{id:"child"},
     [React.createElement('h1',{},"hello from react under 2divs'"), 
-    React.createElement('h2',{},"hello from react under 2divsn and one h1 tag'")]))
+    React.createElement('h2',{},"hello from react under 2divs and one h1 tag'")]))
 console.log(heading)
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(heading)
