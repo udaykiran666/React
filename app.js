e0028f6a455d8d505af7cd81b8130d32533afaa3
udaykiran <udaykiranramaraju@gmail.com>
2024-05-09 00:32:15 +0530

    stopped this episode early, to brush up my css skills

--- app.js
+++ app.js
@@ -1,52 +1,37 @@
 import React from "react";
 import ReactDOM  from "react-dom/client";
+//Header
+//Body
+//Footer
 
-// const heading = React.createElement("h1", {id:"heading"}, "Hello World from React!!")
-// console.log(heading);//at the end of the day react element is js obj..
-// const heading = React.createElement(
-//     'div',{id:"parent"},
-//     React.createElement('div',{id:"child"},
-//     [React.createElement('h1',{},"hello from react under 2divs'"), 
-//     React.createElement('h2',{},"hello from react under 2divs and one h1 tag'")]))
 
-//React Element
-const element = (
-    <h1 class='uday'>
-        Element from react element
-    </h1>
-
-)
-
-//React Component
-const Title = () =>(
-    <h1 className="head" tabIndex="5">
-        React using JSX 
-    </h1>
-)
-
-//React fucntional Component({element} is a react element here)
-const FunctionalComponent = () =>{
+const Header = () =>{
     return (
-    <div id="container1">
-        {element}  
-        <h1 className="parent1">statement of React from FunctionalComponent</h1>
-    </div>)
+    <div className="header">
+        <div className="logo-container">
+            <img 
+            className="logo" 
+            src="https://cdn.dribbble.com/userupload/6440848/file/original-7cc6fd8be265505f2e7b408fb754afa3.png?resize=640x480&vertical=center"></img>
+        </div>
+        <div className="naive-items">
+            <ul>
+                <li>Home</li>
+                <li>About Us</li>
+                <li>Contact Us</li>
+                <li>Cart</li>
+            </ul>
+        </div>
+    </div>
+    )
+}
+const AppLayout = () =>{
+    return (
+    <div className="app">
+        <Header/>
+    </div>
+    )
 }
 
-//another way of writing above functional component 
-const FunctionalComponent2 = () => (
-    <div id="container2">
-        {element}
-        <Title/>
-        <h1 className="parent2">statement of React from FunctionalComponent2</h1>
-    </div>);
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
-console.log(FunctionalComponent())
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(
-    <div>
-      <FunctionalComponent />,
-      <FunctionalComponent2 />
-    </div>
-  );
- 
+root.render(<AppLayout/>);
